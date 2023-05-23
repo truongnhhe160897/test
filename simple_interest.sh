d4c0b14957e88cb87dd04d488377b406f61cbb44
truongnhhe160897 <134406292+truongnhhe160897@users.noreply.github.com>
2023-05-23 21:39:36 +0700

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,32 @@
+as
+dvas
+vr a
+tf
+adsfg 
+asdg 
+adg
+ wd
+ t as
+ df 
+ asf
+ as 
+ fad
+ fgad s
+ g
+  sd
+  hw
+  r tg
+  adfg
+  a
+  dsf a
+  sd 
+  ad
+  f das
+  g fs
+  gh ads
+   f
+   ads 
+   dfgfg
+    ad
+    faS
+    DA
